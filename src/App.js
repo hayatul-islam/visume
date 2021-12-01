351f0a816b3c515094a46bf2fa94805b972597f9
hayatulemon <hayatulemon340@gmail.com>
2021-12-02 00:04:06 +0600

    pricing componet design complate

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import Resume from './components/Resume/Resume';
 import './App.css';
 import GetStarted from './components/GetStarted/GetStarted';
 import About from './components/About/About';
+import Pricing from './components/Pricing/Pricing';
 
 function App() {
   return (
-    <div className="main-container mx-auto">
+    <div className="main-container mx-auto pb-6">
       <Header />
       <Resume />
       <GetStarted />
       <About />
+      <Pricing />
     </div>
   );
 }
---
+++ src/components/Pricing/Pricing.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import './Pricing.css';
+
+const Pricing = () => {
+    return (
+        <div>
+            <div>
+                <p className="small-text-blue">Pricing</p>
+                <h3 className="text-4xl text-blue-800 font-bold mt-3">How much to pay?</h3>
+                <p className="mt-3">You can choose the most app pricing option for your needs.</p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <div className="single-box py-5 shadow px-4 text-center mt-6">
+                    <div className="pt-3">
+                        <h3 className="text-2xl text-blue-800 font-bold">Basic</h3>
+                        <h2 className="text-4xl mt-3 small-text-blue">$8</h2>
+                        <p className="pb-5">per project</p>
+                    </div>
+                    <hr className="mt-4" />
+                    <div className='flex justify-center mt-5'>
+                        <div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>20 themes</span>
+                            </div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>60 music files</span>
+                            </div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>80 audio effects</span>
+                            </div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>8 language support </span>
+                            </div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>Email Support </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-6 py-3">
+                        <button className="pricing-btn py-3 px-8 rounded mr-5">Get Started</button>
+                    </div>
+                </div>
+
+                <div className="single-box py-5 shadow px-4 text-center mt-6">
+                    <div className="pt-3">
+                        <h3 className="text-2xl text-blue-800 font-bold">All Rounder</h3>
+                        <h2 className="text-4xl mt-3 small-text-blue">$12</h2>
+                        <p className="pb-5">per project</p>
+                    </div>
+                    <hr className="mt-4" />
+                    <div className='flex justify-center mt-5'>
+                        <div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>150 themes</span>
+                            </div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>320 music files</span>
+                            </div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>500 audio effects</span>
+                            </div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>80+ language support </span>
+                            </div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>24/7 Support </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-6 py-3">
+                        <button className="pricing-btn py-3 px-8 rounded mr-5">Get Started</button>
+                    </div>
+                </div>
+                <div className=" single-box py-5 shadow px-4 text-center mt-6">
+                    <div className="pt-3">
+                        <h3 className="text-2xl text-blue-800 font-bold">Super Hero</h3>
+                        <p className="mt-6 px-3 mb-7 text-lg">Get in touch with us to discuss on your requirements and pricing.</p>
+                    </div>
+                    <hr className="mt-4" />
+                    <div className='flex justify-center mt-5'>
+                        <div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>Everything From All Rounder</span>
+                            </div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>24/7 Priority Support</span>
+                            </div>
+                            <div className="flex items-center mt-3">
+                                <img className="w-5 h-5 mr-3" src="https://i.ibb.co/TkTy3cy/check-circle.png" alt="" />
+                                <span>And many more..</span>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="mt-6 py-3">
+                        <button className="pricing-btn py-3 px-8 rounded mr-5">Contact Us</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Pricing;
